e8efc112afc921e7a7a4c5853e4ac2eeb3df43e9
khb6997@gmail.com <khb6997@gmail.com>
2023-03-21 11:48:24 +0900

    개발환경 구축

---
+++ src/main/java/com/manager/mountainview/MountainviewApplication.java
@@ -0,0 +1,13 @@
+package com.manager.mountainview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/mountainview/MountainviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.mountainview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
