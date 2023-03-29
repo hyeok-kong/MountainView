06fddacd12ca2d3bf61629c1db173e315317f76e
bonyncloude <wnscjf3062@naver.com>
2023-03-29 11:02:23 +0900

    시작-메인페이지 goodmorning

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -12,22 +12,25 @@ function App() {
                 setMessage(message);
             });
     },[])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {message} </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <div className="logo">
+          <img id="mountainimg" src="mountainviewlogo.PNG" alt="로고 이미지" />
+        </div>
+        <div className="main-text">
+          <h1>Let's Go</h1>
+          <h1><span>Mountain</span></h1>
+        </div>
+        <div className="menu">
+          <ul>
+            <li><a href="index0.html">Home</a></li>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    );
 }
 
 export default App;
