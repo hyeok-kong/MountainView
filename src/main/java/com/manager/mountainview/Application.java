31fbd1a288951419feb94f32d2d7cd84bcfceaf9
khb6997@gmail.com <khb6997@gmail.com>
2023-03-30 23:51:39 +0900

    main 파일 이름 변경

--- src/main/java/com/manager/mountainview/Application.java
+++ src/main/java/com/manager/mountainview/Application.java
@@ -2,12 +2,14 @@ package com.manager.mountainview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class MountainviewApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MountainviewApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
