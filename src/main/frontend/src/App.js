676afbff753ee306fb2c9ad9dbcfe98333f7c061
bonyncloude <wnscjf3062@naver.com>
2023-03-31 14:03:34 +0900

    오류해결

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,32 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    },[])
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {message} </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="logo">
+        <img id="mountainimg" src="mountainviewlogo.PNG" alt="로고 이미지" />
+      </div>
+      <div className="main-text">
+        <h1>Let's Go</h1>
+        <h1><span>Mountain</span></h1>
+      </div>
+      <div className="menu">
+        <ul>
+          <li><a href="/Home">Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
     </div>
+  );
+}
+
+function Login() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
