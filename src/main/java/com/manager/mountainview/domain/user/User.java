d3a3ae407ae380e7660727d2b860c37000437523
khb6997@gmail.com <khb6997@gmail.com>
2023-03-31 14:30:26 +0900

    User Entity 수정

--- src/main/java/com/manager/mountainview/domain/user/User.java
+++ src/main/java/com/manager/mountainview/domain/user/User.java
@@ -2,6 +2,7 @@ package com.manager.mountainview.domain.user;
 
 
 import com.manager.mountainview.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
@@ -18,10 +19,10 @@ public class User extends BaseTimeEntity {
     private Long id;
 
     @Column(length = 200, nullable = false)
-    private String email;
+    private String name;
 
     @Column(length = 200, nullable = false)
-    private String name;
+    private String email;
 
     @Column(nullable = false)
     private Role role;
@@ -38,4 +39,21 @@ public class User extends BaseTimeEntity {
     @ColumnDefault("false")
     @Column(nullable = false)
     private boolean isSubmitted;
+
+    @Builder
+    public User(String name, String email, Role role, String nickname, char gender, int age, boolean isSubmitted) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.age = age;
+        this.isSubmitted = isSubmitted;
+    }
+
+    public User update(String name) {
+        this.name = name;
+
+        return this;
+    }
 }
