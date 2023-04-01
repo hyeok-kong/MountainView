f1f66c14d99e6edff43baff9f84bd857a73ccb4e
khb6997@gmail.com <khb6997@gmail.com>
2023-04-02 01:32:51 +0900

    디렉토리 구조 수정

--- src/main/java/com/manager/mountainview/application/api/helloController.java
+++ src/main/java/com/manager/mountainview/application/api/helloController.java
@@ -1,4 +1,4 @@
-package com.manager.mountainview.hello;
+package com.manager.mountainview.application.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
