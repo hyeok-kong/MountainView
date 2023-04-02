3b065716c625c5133830126efa961337358059b8
khb6997@gmail.com <khb6997@gmail.com>
2023-04-02 23:07:28 +0900

    클래스명 수정

--- src/main/java/com/manager/mountainview/application/api/HelloController.java
+++ src/main/java/com/manager/mountainview/application/api/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloController {
+public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
         return "hello world";
