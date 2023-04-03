8249494e6e906300b153c60aedf0c2aff5a66dd0
HwanSeok Yong <101648597+yonghwanseok@users.noreply.github.com>
2023-04-04 02:58:53 +0900

    Footer 컴포넌트

---
+++ src/main/frontend/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../../App";
+import './Footer.css'
+
+
+function Footer(props) {
+  return (
+    <footer>
+      <footer className='footerBox'>
+        <h1 class ="footertext"> Maintainview   </h1>
+    </footer>
+    </footer>
+    
+  );
+}
+
+export default Footer
