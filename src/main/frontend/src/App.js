31c6c9ff0606be5d5fe1820062ff53b5eaf97bbe
bonyncloude <wnscjf3062@naver.com>
2023-04-03 10:48:38 +0900

    kakao sdk 써서 mainpage 구성해봄

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.css'; // Import the CSS file
 import icon from './mountainviewlogo.PNG'
+import { Map, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk';
 
+function Main() {
+  const [markerPosition, setMarkerPosition] = useState({ lat: 37.5665, lng: 126.9780 });
+  const [overlayPosition, setOverlayPosition] = useState({ lat: 37.5665, lng: 126.9780 });
+  const [overlayVisibility, setOverlayVisibility] = useState(false);
+
+  const handleMarkerClick = () => {
+    setOverlayVisibility(true);
+  };
+
+  const handleOverlayCloseClick = () => {
+    setOverlayVisibility(false);
+  };
+
+  return (
+    <div className="home-page">
+      <div className="logo">
+        <img className="logo-img" src={icon} alt="Logo Image" />
+      </div>
+      <div className="menu">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/main">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <Map center={{ lat: 37.5665, lng: 126.9780 }} style={{ height: '500px' }}>
+        <MapMarker position={markerPosition} onClick={handleMarkerClick} />
+        <CustomOverlayMap
+          position={overlayPosition}
+          visible={overlayVisibility}
+          onClick={handleOverlayCloseClick}
+        >
+          <div>
+            <p>Hiking Trail Information</p>
+            <p>Difficulty: Medium</p>
+            <p>Length: 5km</p>
+          </div>
+        </CustomOverlayMap>
+      </Map>
+    </div>
+  );
+}
 
 function Home() {
   return (
-    <div className="home-page"> {/* Add class name here */}
+    <div className="home-page">
       <div className="logo">
-        <img id="mountainimg" src={icon} alt="로고 이미지" />
+        <img className="logo-img" src={icon} alt="로고 이미지" />
       </div>
       <div className="main-text">
         <h1>Let's Go</h1>
@@ -16,16 +60,15 @@ function Home() {
       </div>
       <div className="menu">
         <ul>
-          <li><a href="/Home">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="/login">Login</a></li>
-          <li><a href="#">Services</a></li>
+          <li><a href="/main">Services</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </div>
     </div>
   );
 }
-
 function Login() {
   const [message, setMessage] = useState("");
   useEffect(() => {
@@ -42,13 +85,13 @@ function Login() {
     </div>
   );
 }
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
   );
