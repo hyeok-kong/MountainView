bf64581a21844952cb8a81ddfb4f7e49afd80800
khb6997@gmail.com <khb6997@gmail.com>
2023-04-08 16:21:29 +0900

    Entity 생성

---
+++ src/main/java/com/manager/mountainview/domain/mountain/Mountain.java
@@ -0,0 +1,44 @@
+package com.manager.mountainview.domain.mountain;
+
+import com.manager.mountainview.application.dto.response.MountainResponseDto;
+import com.manager.mountainview.domain.spot.Spot;
+import com.manager.mountainview.domain.trail.Trail;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Mountain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private Point2D.Double location;
+
+    @OneToMany(mappedBy = "mountain")
+    private List<Spot> spots = new ArrayList<>();
+
+    @OneToMany(mappedBy = "mountain")
+    private List<Trail> trails = new ArrayList<>();
+
+    @Builder
+    public Mountain(String code, String name, Point2D.Double location) {
+        this.code = code;
+        this.name = name;
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/manager/mountainview/domain/trail/Trail.java
@@ -0,0 +1,53 @@
+package com.manager.mountainview.domain.trail;
+
+import com.manager.mountainview.domain.mountain.Mountain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.geom.Point2D;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Trail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double length;
+
+    private String difficult;
+
+    // 상행시간
+    private int uppl;
+    // 하행시간
+    private int godn;
+    // 통제여부
+    private char cnrl;
+    // 폐쇄여부
+    private char cls;
+    // 위험구간내용
+    private String risk;
+    // 등산로 시작좌표
+    private Point2D.Double start;
+    // 등산로 끝좌표
+    private Point2D.Double end;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mountain_code")
+    private Mountain mountain;
+
+//    @Builder
+//    public Trail(String name, int length, String difficult, int uppl, int godn,
+//    char cncl, char cls, String risk, Point2D.Double start, Point2D.Double end, Mountain mountain)
+}
