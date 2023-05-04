408b1f932684aa93df7e03e2c75c2ca62989f580
khb6997@gmail.com <khb6997@gmail.com>
2023-05-04 18:21:49 +0900

    시간 포맷 수정

--- src/main/java/com/manager/mountainview/application/service/Formatter.java
+++ src/main/java/com/manager/mountainview/application/service/Formatter.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Formatter {
     public static String localDateTimeToString(LocalDateTime time) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd/HH/mm");
         String result = time.format(formatter);
 
         return result;
