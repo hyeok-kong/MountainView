2e733206b04d671cd75694d7876d9bd1b30ed383
khb6997@gmail.com <khb6997@gmail.com>
2023-05-17 00:57:08 +0900

    JSON타입 반환

--- src/main/java/com/manager/mountainview/application/api/ChatGPTController.java
+++ src/main/java/com/manager/mountainview/application/api/ChatGPTController.java
@@ -4,8 +4,9 @@ package com.manager.mountainview.application.api;
 import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,18 @@ public class ChatGPTController {
     private final ChatgptService chatgptService;
 
     @PostMapping("/api/chat")
-    public String aiResponse(@RequestBody String request) {
-        return chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user", request)));
+    public JSONObject aiResponse(@RequestBody String request) {
+        String response = chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user", request)));
+        JSONObject result = new JSONObject();
+
+        JSONParser parser = new JSONParser();
+        try {
+            result = (JSONObject) parser.parse(response);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
